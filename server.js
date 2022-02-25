782f5da588fee8daccbd8c823aaedbe92327ed68
K3A101 <keisha.alexander@hva.nl>
2022-02-25 21:39:18 +0100

    Express Hello world oefening

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
 
+
+/***************************************/
+/* Define some constants and variables */
+/***************************************/
+const app = express();
+const port = 3000;
+
+/**********/
+/* Route  */
+/**********/
 app.get('/', (req, res) => {
+    // een respons functie
     res.send('Hello World!')
 })
+//site root
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
 
 app.get('/about', (req, res) => {
     res.send('Dit page is about me')
 })
 
-app.use('/', )
+
+
+/*******************/
+/* Start webserver */
+/*******************/
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Web server running on http://localhost:${port}`)
+});
+// ga luisteren naar alle verzoeken in port 3000
